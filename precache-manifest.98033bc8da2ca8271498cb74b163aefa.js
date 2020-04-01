75051856c4897a47329296184b9e0361e6ce2180
Vasyl-007 <vasyl.manyo@gmail.com>
2020-04-01 15:38:48 +0300

    Updates

---
+++ precache-manifest.98033bc8da2ca8271498cb74b163aefa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3921c9a9977e6625b62af7851f76be3",
+    "url": "/gh-pages/index.html"
+  },
+  {
+    "revision": "db0d923887960f1b7e64",
+    "url": "/gh-pages/static/css/main.ee343f66.chunk.css"
+  },
+  {
+    "revision": "8f16cf3f7ee068e26cdc",
+    "url": "/gh-pages/static/js/2.45f18323.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages/static/js/2.45f18323.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db0d923887960f1b7e64",
+    "url": "/gh-pages/static/js/main.19c04272.chunk.js"
+  },
+  {
+    "revision": "6a89e067e2bd59b1a4f6",
+    "url": "/gh-pages/static/js/runtime-main.ef614d5f.js"
+  }
+]);
